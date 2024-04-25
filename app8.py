48e0561e87224f3837bde157aa05542121e8726d
Bokddung <165736241+Bokddung@users.noreply.github.com>
2024-04-25 16:09:30 +0900

    1.1

--- app8.py
+++ app8.py
@@ -6,44 +6,21 @@ from app8_eda import run_eda
 from app8_ml import run_ml
 from app8_about import run_about
 
-def main() :
+def main():
     st.title('파일 분리 앱')
 
-    menu = ['Home','EDA','ML','About']
+    menu = ['Home', 'EDA', 'ML', 'About']
 
-    choice = st.sidebar.selectbox('메뉴',menu)
+    choice = st.sidebar.selectbox('메뉴', menu)
 
     if choice == menu[0]:
         run_home()
-
     elif choice == menu[1]:
         run_eda()
-
     elif choice == menu[2]:
         run_ml()
-
     elif choice == menu[3]:
         run_about()
 
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__' :
+if __name__=='__main__' :
     main()
-
-
-
-
-
-
-
-
-
-
